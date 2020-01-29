a1c5ffd8dd84b32116aa0a557135fadeab830831
piyushbhutani1999 <piyushbhutaniynr@gmail.com>
2020-01-30 00:59:14 +0530

    sending data in json using pure Django

--- cfseapi/urls.py
+++ cfseapi/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from updates.views import update_models_detail_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', update_models_detail_view, name = "home")
 ]
--- updates/views.py
+++ updates/views.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+
+#render is a shortcut to http templte response
+# render(templateName, **CONTEXT**)
+# render = HttpResponse(get_template().render({**CONTEXT**}))
+from django.shortcuts import render 
 
 # Create your views here.
+from .models import Updates
+from django.http import JsonResponse, HttpResponse
+
+
+def update_models_detail_view(request):
+    data = {
+        "count" : 1000,
+        "content" : "here comes content"
+    }
+    #JsonResponse convert python dict to JS objects notation(JSON)
+    return JsonResponse(data)
